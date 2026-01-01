ec49a804ebc520e58b77ca5b5b6e29f4e5f58a4d
Nugraha Yudhasyah <nugrahayudhasyah@sttcipasung.ac.id>
2026-01-01 21:52:01 +0700

    feat: Jobs Page and Card Component

--- www/app/routes/web.php
+++ www/app/routes/web.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('jobs', JobController::class)->only(['index']);
+
+Route::get('', function (){
+    return to_route('jobs.index');
+});
