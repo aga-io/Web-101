061a5f64f2aee2434f4b92b6c25274cecaab1aa9
Nugraha Yudhasyah <nugrahayudhasyah@sttcipasung.ac.id>
2026-01-01 22:56:38 +0700

    feat: Refactor: Refactoring Filtering Backend Logic

--- www/app/resources/views/components/text-input.blade.php
+++ www/app/resources/views/components/text-input.blade.php
@@ -1,7 +1,7 @@
 <div class="relative">
-    @if($formId)
+    @if($formRef)
         <button type="button" class="absolute top-0 right-0 flex h-full items-center pr-2"
-                onclick="document.getElementById('{{ $name }}').value= ''; document.getElementById('{{ $formId }}').submit()">
+                @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit()">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                  stroke="currentColor"
                  class="h-4 w-4 text-slate-500">
@@ -9,6 +9,6 @@
             </svg>
         </button>
     @endif
-    <input type="text" placeholder="{{ $placeholder }}" name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"
-           class="w-full rounded-md border-0 py-1.5 px-2.5 pr-8 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2"/>
+        <input x-ref="input-{{ $name }}" type="text" placeholder="{{ $placeholder }}" name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"
+               class="w-full rounded-md border-0 py-1.5 px-2.5 pr-8 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2"/>
 </div>
