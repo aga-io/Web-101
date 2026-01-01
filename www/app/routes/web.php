4e75aeeb8ee76baeb9e5bc038d855ea23d27f427
Nugraha Yudhasyah <nugrahayudhasyah@sttcipasung.ac.id>
2026-01-01 21:59:35 +0700

    feat: Job Page: Job Card & Link Button Components

--- www/app/routes/web.php
+++ www/app/routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('jobs', JobController::class)->only(['index']);
+Route::resource('jobs', JobController::class)->only(['index', 'show']);
 
 Route::get('', function (){
     return to_route('jobs.index');
