771c8efe0f41dfb32b084134ca68d268b79f3b5e
Nugraha Yudhasyah <nugrahayudhasyah@sttcipasung.ac.id>
2026-01-02 11:11:06 +0700

    feat: Blade Component and Layout

--- www/app/database/factories/JobFactory.php
+++ www/app/database/factories/JobFactory.php
@@ -19,7 +19,11 @@ class JobFactory extends Factory
     {
         return [
             'title' => $this->faker->jobTitle,
-            'description' => $this->faker->paragraphs(3, true)
+            'description' => $this->faker->paragraphs(3, true),
+            'salary' => $this->faker->numberBetween(5_000, 150_000),
+            'location' => $this->faker->city,
+            'category' => $this->faker->randomElement(Job::$category),
+            'experience' => $this->faker->randomElement(Job::$experience)
         ];
     }
 }
