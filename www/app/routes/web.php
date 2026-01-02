ad904a6505f6804f40e02d74202e52bc250a5ad4
Nugraha Yudhasyah <nugrahayudhasyah@sttcipasung.ac.id>
2026-01-02 11:25:02 +0700

    feat: Job Page: Tag Component and Job Info

--- www/app/routes/web.php
+++ www/app/routes/web.php
@@ -6,3 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::resource(
     'jobs',
     JobController::class)->only(['index']);
+
+Route::get('', function (){
+    return to_route('jobs.index');
+});
